88a4f93cca3c71257ae71ae2ba15253f41bed601
KunalTheEngineer <kunalbthakare22@gmail.com>
2024-09-16 08:43:03 +0530

    Add files via upload

---
+++ Turf Booking System/src/turf/booking/system/TurfBookingSystem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package turf.booking.system;
+
+public class TurfBookingSystem {
+
+    /**
+         @param args the command line arguments
+     **/
+    
+    public static void main(String[] args) 
+    {
+        MainScreen ms = new MainScreen();
+        ms.setLocationRelativeTo(null);
+        ms.setVisible(true);
+    }
+    
+}
+
